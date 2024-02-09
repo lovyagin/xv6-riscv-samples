18f2495d2168c413c5be97cb26e403f822892cce
Nikita Yu. Lovyagin <lovyagin@mail.com>
2024-02-09 21:07:32 +0300

    some clean-ups

--- user/hello.c
+++ user/hello.c
@@ -5,11 +5,18 @@
 int
 main(int argc, char *argv[])
 {
-
-  char msg[] = "Hello world (write)\n";
-  write(1, msg, sizeof(msg) - 1); // Excluding trailing \0
-
-  printf("Hello world (printf)\n");
+  printf("Hello, world (printf)\n");      // Common printf-based Hello, world
   
-  exit(0);
+  char msg[] = "Hello, world (write)\n";  // Buffer to write
+  int len = sizeof(msg) - 1;              // Message length excluding trailing '\0'
+
+  int ret = write(1, msg, len);           // Write to stdout
+  
+  if (ret == len)                               // Check for write return
+      exit (0);                                 // Exit success
+  else            
+  {
+      write (2, "Write failure (stdout)\n", 23); // Write to stderr (bad: 23 is a magic number here)
+      exit (1);                                  // Exit failure
+  }
 }
