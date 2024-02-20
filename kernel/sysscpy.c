2a30387fd00d2a15123f00643deda82bb8f778b3
Nikita Yu. Lovyagin <lovyagin@mail.com>
2024-02-20 20:49:48 +0300

    Check NULL

--- kernel/sysscpy.c
+++ kernel/sysscpy.c
@@ -12,9 +12,14 @@ uint64 sys_scpy(void)
 {
 
     uint64 dst;
-    argaddr(0, &dst);  // first argument: dst address
+    argaddr(0, &dst);              // first argument: dst address
+    if (dst == 0) return -3;       // Check for NULL, error code, dst buffer invalid
 
-    char buf[BUF_LEN]; // kernel buffer for source string
+    uint64 src;
+    argaddr(1, &src);              // second argument: src address, just to check for NULL
+    if (src == 0) return -2;       // error code: src buffer invalid
+
+    char buf[BUF_LEN];             // kernel buffer for source string
     int len;
     len = argstr(1, buf, BUF_LEN); // second argument: source string
     if (len < 0) return -1;        // error code, src string invalid
