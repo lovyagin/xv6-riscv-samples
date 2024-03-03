324a723b821c1071e125a456c59aec68e283e1f4
DungeonMaster <st106332@student.spbu.ru>
2024-03-03 20:45:54 +0300

    Fixed read version

--- user/add_read.c
+++ user/add_read.c
@@ -12,7 +12,7 @@ void readToEnd() {
 
 void errorMessage(const char* msg) {
   write(2, msg, strlen(msg));
-  exit(0);
+  exit(-1);
 }
 
 void errorMessageAndReadToEnd(const char* msg) {
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
       x = x * 10 + c - '0';
       cntRead++;
     } else if (c == ' ') break;
-    else if (c == '\n') errorMessage("Incorrect format\n");
+    else if (c == '\n') errorMessage("Expected two integers, but given only one\n");
     else errorMessageAndReadToEnd("Incorrect format\n");
   }
   if (cntRead == 0) errorMessageAndReadToEnd("First number not found\n");
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
   cntRead = 0;
   for (;;) {
     cc = read(0, &c, 1);
-    if (cc < 1) errorMessage("Incorrect format\n");
+    if (cc < 1) break;
     if (c >= '0' && c <= '9') {
       y = y * 10 + c - '0';
       cntRead++;
@@ -54,5 +54,5 @@ int main(int argc, char *argv[])
   if (cntRead == 0) errorMessage("Second number not found\n");
   
   printf("%d\n", x + y);
-  return 0;
+  exit(0);
 }
