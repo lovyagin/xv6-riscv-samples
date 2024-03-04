2a4393ef81d977c1da9c989b80669b3f84c2e2df
Nikita Yu. Lovyagin <lovyagin@mail.com>
2024-03-04 12:41:42 +0300

    add errno

---
+++ kernel/errno.h
@@ -0,0 +1,5 @@
+#define ESUCCESS 0 // No error
+#define ENULL    1 // NULL userspace address
+#define EFAULT   2 // Bad userspace address
+#define ENOMEM   3 // Not enough kernel memory
+#define ESIZE    4 // Bad buffer size
